e45b588ee922a0d1cf0bcecf83208a25d3d78aa5
Willian Fakri Bratkauskas <willfakri@gmail.com>
2023-10-08 15:43:09 -0300

    Adicionado imposto ICCC pelo padrao strategy

--- Program.cs
+++ Program.cs
@@ -5,14 +5,16 @@ namespace CursoDesignPatterns
     {
         static void Main(string[] args)
         {
-            var orcamento = new Orcamento(10);
+            var orcamento = new Orcamento(10000);
             var calculador = new CalculadorDeImpostos();
 
             IImposto icms = new ICMS();
             IImposto iss = new ISS();
+            IImposto iccc = new ICCC();
 
             calculador.RealizaCalculo(orcamento, icms);
             calculador.RealizaCalculo(orcamento, iss);
+            calculador.RealizaCalculo(orcamento, iccc);
 
             Console.ReadKey();
         }
